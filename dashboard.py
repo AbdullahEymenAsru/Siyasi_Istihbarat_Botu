98acd7db193239491ddf91ccd650673175abc6ea
Abdullah Eymen Asru <75729487+AbdullahEymenAsru@users.noreply.github.com>
2025-12-17 16:02:53 +0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -59,7 +59,7 @@ for folder in ["ARSIV", "VEKTOR_DB"]:
 # -- YENİ MANUEL EMBEDDING SINIFI ---
 
 class YerelEmbedder:
-       def **init**(self):
+def __init__(self):
 self.model = SentenceTransformer("all-MiniLM-L6-v2", device="cpu")
 def **call**(self, input):
 return self.model.encode(input).tolist()
